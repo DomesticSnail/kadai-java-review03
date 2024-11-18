9c23807f1bf50e4a52a22c02f766c15ebf8bbf79
DomesticSnail <philliptowler94@gmail.com>
2024-11-19 01:46:46 +0900

    ２回目のコミット

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -2,56 +2,26 @@ package baseball;
 
 public class BaseBallTeam {
 
-    private String name;
-    private int win;
-    private int lose;
-    private int draw;
+    public String name;
+    public int win;
+    public int lose;
+    public int draw;
+
+    public BaseBallTeam() {
+ }
 
     public BaseBallTeam(String name, int win, int lose, int draw) {
         this.name = name;
         this.win = win;
         this.lose = lose;
-        this.draw = draw;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getWin() {
-        return win;
-    }
-
-    public void setWin(int win) {
-        this.win = win;
-    }
-
-    public int getLose() {
-        return lose;
-    }
-
-    public void setLose(int lose) {
-        this.lose = lose;
-    }
-
-    public int getDraw() {
-        return draw;
-    }
-
-    public void setDraw(int draw) {
-        this.draw = draw;
-    }
-
+        this. draw = draw;
+}
     public double getRate() {
         return (double) win / (win + lose);
     }
+        public void report() {
+        System.out.println(this.name + "の2022年の成績は" + this.win + "勝" + this.lose + "敗" + this.draw + "分、勝率は" + this.getRate() + "です。");
 
-    public void report() {
-        System.out.printf("%s の2022年の成績は %d勝 %d敗 %d分、勝率は %.15fです。\n",
-                name, win, lose, draw, getRate());
     }
+
 }
